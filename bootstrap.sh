15773d94667f8ca8b1f0e38c803d2600365616e2
eric vz <evonzee@themxgroup.com>
2018-04-30 17:26:26 -0500

    use gigadot for java download too

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,7 @@ yum install wget -y
 
 echo "Installing Java 8.."
 cd /opt/
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz"
+wget --progress=dot:giga --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz"
 tar xzf jdk-8u131-linux-x64.tar.gz
 cd /opt/jdk1.8.0_131/
 alternatives --install /usr/bin/java java /opt/jdk1.8.0_131/bin/java 2
